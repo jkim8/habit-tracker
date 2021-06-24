90f5baae21802b717f7070d4c048d779808a6bcc
naujkim <juno0218@naver.com>
2021-06-24 09:18:07 -0400

    PureComponent

--- src/app.jsx
+++ src/app.jsx
@@ -17,23 +17,28 @@ export class App extends Component {
 
 
 handleIncrement = (habit) => {
-  console.log(`handleIncrement ${habit.name}`);
-  const habits = [...this.state.habits]
-  const index = habits.indexOf(habit)
-  habits[index].count++
+  const habits = this.state.habits.map(item => {
+    if(item.id === habit.id) {
+      return {...habit, count: habit.count +1}
+    } 
+    return item;
+  })
   this.setState({ habits })
 
 }
 
 handleDecrement = (habit) => {
-  console.log(`handleDecrement ${habit.name}`);
-  const habits = [...this.state.habits]
-  const index = habits.indexOf(habit)
-  const count = habits[index].count -1
-  habits[index].count = count < 0 ? 0 : count
+  const habits = this.state.habits.map(item => {
+    if(item.id === habit.id) {
+      const count = habit.count -1
+      return {...habit, count: count < 0 ? 0 : count}
+    } 
+    return item;
+  })
   this.setState({ habits })
 
 }
+
 
 handleDelete = (habit) => {
   console.log(`handleDelete ${habit.name}`);
@@ -48,8 +53,10 @@ handleAdd = name => {
 
 handelReset = () => {
   const habits = this.state.habits.map(habit => {
-    habit.count = 0;
-    return habit;
+    if(habit.count !== 0) {
+      return {...habit, count: 0}
+    }
+    return habit
   })
   this.setState({ habits })
 }
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -32,6 +32,8 @@ import HabitAddForm from './habitAddForm'
                             <Habit 
                                 key={habit.id} 
                                 habit={habit} 
+                                name={habit.name}
+                                count={habit.count}
                                 onIncrement={this.handleIncrement} 
                                 onDecrement={this.handleDecrement} 
                                 onDelete={this.handleDelete} 
